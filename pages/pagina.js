0b97b91d171f8731ffa688f138726e17e2e5f81e
AkirahX <akirah.ofc@gmail.com>
2021-09-25 21:02:53 -0300

    Pagina

---
+++ pages/pagina.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Pagina.module.css'
+
+function Pagina (){
+        
+    return (
+    <div>
+        <div className={styles.item}>
+            ROI
+        </div>
+    </div>)
+}
+
+export default Pagina
